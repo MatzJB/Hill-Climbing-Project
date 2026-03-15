a7778f78cf6d4646e9406f456e25c87833cca308
MatzJB <matz.johansson@gmail.com>
2026-03-14 23:02:10 -0600

    fixed HCA

--- src/cpp/HCA/HCA/HCA.cpp
+++ src/cpp/HCA/HCA/HCA.cpp
@@ -11,12 +11,17 @@
 #define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
 #include <iostream>
 
-#include "math.h"
+#include <cmath>
 #include <algorithm>
 #include <string>   
 #include <time.h> 
 
 #include <chrono>
+#include <climits>
+
+//#include "matplotlibcpp.h"
+
+
 
 using namespace std;
 
@@ -38,15 +43,15 @@ typedef struct {
 } polyElement;
 
 
-rgba get_pixel(unsigned char *img, int channels, int img_width, int img_height, int x, int y)
+rgba get_pixel(unsigned char* img, int channels, int img_width, int img_height, int x, int y)
 {
-	int coordinate = (x + img_width*y)*channels;
+	int coordinate = (x + img_width * y) * channels;
 	rgba col;
-
+	col.a = 0;
 	col.r = *(img + coordinate);
 	col.g = *(img + coordinate + 1);
 	col.b = *(img + coordinate + 2);
-	
+
 	if (channels == 4)
 	{
 		col.a = *(img + coordinate + 3);
@@ -55,35 +60,26 @@ rgba get_pixel(unsigned char *img, int channels, int img_width, int img_height, 
 	return col;
 }
 
-
-void add_pixel(unsigned char *img, int channels, int img_width, int img_height, int x, int y, int r, int g, int b, int a)
+// change to put_pixel 
+void add_pixel(unsigned char* img, int channels, int img_width, int img_height, 
+	int x, int y, 
+	int r, int g, int b, int a)
 {
 	// image is layed out in memory row-wise [rgba, rgba], [row 1, row 2, row 3,..., row height]
 	// upper left corner is (0,0)
-	int coordinate = (x + img_width*y)*channels; // works fine
-	//float alpha_image = *(img + coordinate + 3) / 255.0f;
-	//float alpha_added = a / 255.0f;
+	int coordinate = (x + img_width * y) * channels; // works fine
+	float a_disc = a / 255.0f; 
+	float a_img = *(img + coordinate + 3) / 255.0f;
 
-	if (channels == 4)
-	{
-		//*(img + coordinate + 3) = (alpha_image + alpha_added*(1 - alpha_image))*255.0f; // alpha
-		//*(img + coordinate + 3) = (alpha_image + alpha_added*(1 - alpha_image))*255.0f;
-		*(img + coordinate + 3) = 255.0f;
+	float rr = *(img + coordinate) * (1-a_disc) + a_disc *r;
+	float gg = *(img + coordinate + 1)*(1- a_disc) + (a_disc) * g;
+	float bb = *(img + coordinate + 2) * (1- a_disc) + (a_disc) * b;
 
-	}
+	*(img + coordinate) = int(rr);
+	*(img + coordinate + 1) = int(gg);
+	*(img + coordinate + 2) = int(bb);
+	*(img + coordinate + 3) = 255.0f * (a_disc + a_img*(1 - a_disc));
 
-	//*(img + coordinate) = min(alpha_image*(*(img + coordinate)) + (unsigned char)r*(1 - alpha_image), 255.0f); // red
-	//*(img + coordinate + 1) = min(alpha_image*(*(img + coordinate + 1)) + (unsigned char)g*(1 - alpha_image), 255.0f); // green
-	//*(img + coordinate + 2) = min(alpha_image*(*(img + coordinate + 2)) + (unsigned char)b*(1 - alpha_image), 255.0f); // blue
-	*(img + coordinate) = r;
-	*(img + coordinate + 1) = g;
-	*(img + coordinate + 2) = b;
-
-
-	/*for (int i = 0; i < 3; i++)
-	{
-		*(img + coordinate + i) = min(*(img + coordinate + i)*1.0f, 255.0f);
-	}*/
 }
 
 // distance from xy to oxy
@@ -92,7 +88,7 @@ float distance(int x, int y, int ox, int oy)
 	return sqrt(pow(x - ox, 2.0f) + pow(y - oy, 2.0f));
 }
 
-void add_disc(unsigned char *img, int channels,
+void add_disc(unsigned char* img, int channels,
 	int img_width, int img_height, polyElement pe)
 {
 	int x = pe.x;
@@ -103,9 +99,9 @@ void add_disc(unsigned char *img, int channels,
 	int b = pe.col.b;
 	int a = pe.col.a;
 
-	for (int i = x - radius; i < x + radius; i++)
+	for (int j = y - radius; j < y + radius; j++)
 	{
-		for (int j = y - radius; j < y + radius; j++)
+		for (int i = x - radius; i < x + radius; i++)
 		{
 			if (i < img_width && i >= 0 && j >= 0 && j < img_height)
 			{
@@ -117,53 +113,194 @@ void add_disc(unsigned char *img, int channels,
 		}
 	}
 }
+//
+//// maybe add argument to add disc and only focusing on that part of the image for diff
+//float diff(unsigned char* img, unsigned char* img2, int channels,
+//	int img_width, int img_height)
+//{
+//	float diff = 0;
+//	for (int y = 0; y < img_height; y++)
+//	{
+//		for (int x = 0; x < img_width; x++)
+//		{
+//			rgba col_a = get_pixel(img, channels, img_width, img_height, x, y);
+//			rgba col_b = get_pixel(img2, channels, img_width, img_height, x, y);
+//
+//			float bn = col_b.a / 255.0;
+//			float rr = col_a.r * (1-bn) + ( bn) * col_b.r;
+//			float gg = col_a.g * (1-bn) +  (bn) * col_b.g;
+//			float bb = col_a.b * (1-bn) + ( bn) * col_b.b;
+//
+//			diff += fabs(col_a.r - col_b.r) + fabs(col_a.g - col_b.g) + fabs(col_a.b - col_b.b);
+//
+//			//printf("(%d %d) %f\n", x, y, diff);
+//		}
+//	}
+//
+//	return diff;
+//}
+//
+//
+//void diff_images(unsigned char* img, unsigned char* img2, unsigned char* img3, int channels,
+//	int img_width, int img_height)
+//{
+//
+//	for (int x = 0; x < img_width; x++)
+//	{
+//		for (int y = 0; y < img_height; y++)
+//		{
+//			rgba col_a = get_pixel(img, channels, img_width, img_height, x, y);
+//			rgba col_b = get_pixel(img2, channels, img_width, img_height, x, y);
+//			rgba col_diff;
+//			int r = abs(col_a.r - col_b.r);
+//			int g = abs(col_a.g - col_b.g);
+//			int b = abs(col_a.b - col_b.b);
+//			int a = abs(col_a.a - col_b.a);
+//
+//			add_pixel(img3, channels, img_width, img_height,
+//				x, y, r,
+//				g, b, a);
+//		}
+//	}
+//}
 
-//maybe add argument to add disc and only focusing on that part of the image for diff
-float diff(unsigned char *img, unsigned char*img2, int channels,
-	int img_width, int img_height)
+
+// 3 seconds
+float add_disc_diff_opt(unsigned char* img_in,
+	unsigned char* img_buff,
+	int channels,
+	polyElement pe_tmp,
+	int width,
+	int height)
+{
+	float diff = 0.0f;
+
+	const int cx = pe_tmp.x;
+	const int cy = pe_tmp.y;
+	const int radius = pe_tmp.radius;
+	const int radius2 = radius * radius;
+
+	const float an = pe_tmp.col.a / 255.0f;
+	const float inv_an = 1.0f - an;
+
+	const float disc_r = (float)pe_tmp.col.r;
+	const float disc_g = (float)pe_tmp.col.g;
+	const float disc_b = (float)pe_tmp.col.b;
+
+	const int min_x = max(0, cx - radius);
+	const int max_x = min(width - 1, cx + radius - 1);
+	const int min_y = max(0, cy - radius);
+	const int max_y = min(height - 1, cy + radius - 1);
+
+	for (int j = min_y; j <= max_y; ++j)
+	{
+		const int dy = j - cy;
+		const int dy2 = dy * dy;
+		const int row = j * width;
+
+		for (int i = min_x; i <= max_x; ++i)
+		{
+			const int dx = i - cx;
+			if (dx * dx + dy2 >= radius2)
+				continue;
+
+			const int coordinate = (row + i) * channels;
+
+			const float in_r = (float)img_in[coordinate];
+			const float in_g = (float)img_in[coordinate + 1];
+			const float in_b = (float)img_in[coordinate + 2];
+
+			const float buff_r = (float)img_buff[coordinate];
+			const float buff_g = (float)img_buff[coordinate + 1];
+			const float buff_b = (float)img_buff[coordinate + 2];
+
+			const float tmp_r = buff_r * inv_an + disc_r * an;
+			const float tmp_g = buff_g * inv_an + disc_g * an;
+			const float tmp_b = buff_b * inv_an + disc_b * an;
+
+			const float dr_old = in_r - buff_r;
+			const float dg_old = in_g - buff_g;
+			const float db_old = in_b - buff_b;
+
+			const float dr_new = in_r - tmp_r;
+			const float dg_new = in_g - tmp_g;
+			const float db_new = in_b - tmp_b;
+
+			const float old_err = dr_old * dr_old + dg_old * dg_old + db_old * db_old;
+			const float new_err = dr_new * dr_new + dg_new * dg_new + db_new * db_new;
+
+			diff += (new_err - old_err);
+		}
+	}
+
+	return diff;
+}
+
+// 21 seconds
+float add_disc_diff(unsigned char* img_in, unsigned char* img_buff, int channels, polyElement pe_tmp, int width, int height)
 {
 	float diff = 0;
-	for (int x = 0; x < img_width; x++)
+	for (int i = pe_tmp.x - pe_tmp.radius; i < pe_tmp.x + pe_tmp.radius; i++)
 	{
-		for (int y = 0; y < img_height; y++)
+		for (int j = pe_tmp.y - pe_tmp.radius; j < pe_tmp.y + pe_tmp.radius; j++)
 		{
-			volatile rgba col_a = get_pixel(img, channels, img_width, img_height, x, y);
-			volatile rgba col_b = get_pixel(img2, channels, img_width, img_height, x, y);
-			//float diff_now = fabs(col_a.r - col_b.r) / 255.0f + fabs(col_a.g - col_b.g) / 255.0f + fabs(col_a.b - col_b.b) / 255.0f + fabs(col_a.a - col_b.a) / 255.0f;
-			volatile float diff_now = abs(col_a.r - col_b.r) + abs(col_a.g - col_b.g) + abs(col_a.b - col_b.b);
+				if (i >= 0 && j >= 0 && i < width && j < height) // boundary check
+				{
+					rgba col_in = get_pixel(img_in, channels, width, height, i, j);
+					rgba col_buff = get_pixel(img_buff, channels, width, height, i, j);
+                    rgba col_tmp;
+					float an = pe_tmp.col.a / 255.0f;
+					// fix this
 
-			diff += diff_now;
-			//printf("(%d %d) %f\n", x, y, diff_now);
+					if (distance(pe_tmp.x, pe_tmp.y, i, j) < pe_tmp.radius)
+					{
+                        col_tmp.r = col_buff.r * (1 - an) + an * pe_tmp.col.r;
+                        col_tmp.g = col_buff.g * (1 - an) + an * pe_tmp.col.g;
+                        col_tmp.b = col_buff.b * (1 - an) + an * pe_tmp.col.b;
+					}
+					else //outside of disc but getting image template
+					{
+						continue;
+					}
+
+
+					float old_err =
+						fabs((float)col_in.r - col_buff.r) +
+						fabs((float)col_in.g - col_buff.g) +
+						fabs((float)col_in.b - col_buff.b);
+
+					float new_err =
+						fabs((float)col_in.r - col_tmp.r) +
+						fabs((float)col_in.g - col_tmp.g) +
+						fabs((float)col_in.b - col_tmp.b);
+
+					diff += (new_err - old_err);
+
+
+                    //diff += fabs(col_in.r - col_tmp.r) + fabs(col_in.g - col_tmp.g) + fabs(col_in.b - col_tmp.b);
+				}
 		}
 	}
 	return diff;
 }
 
 
-
-void diff_images(unsigned char *img, unsigned char*img2, unsigned char*img3, int channels,
-	int img_width, int img_height)
+int random(int min, int max) //range : [min, max]
 {
-
-	for (int x = 0; x < img_width; x++)
+	static bool first = true;
+	if (first)
 	{
-		for (int y = 0; y < img_height; y++)
-		{
-			rgba col_a = get_pixel(img, channels, img_width, img_height, x, y);
-			rgba col_b = get_pixel(img2, channels, img_width, img_height, x, y);
-			rgba col_diff;
-			int r = abs(col_a.r - col_b.r);
-			int g = abs(col_a.g - col_b.g);
-			int b = abs(col_a.b - col_b.b);
-			int a = abs(col_a.a - col_b.a);
-
-			add_pixel(img3, channels, img_width, img_height,
-				x, y, r,
-				g, b, a);
-		}
+		srand(time(NULL)); //seeding for the first time only!
+		first = false;
 	}
+	return min + rand() % ((max + 1) - min);
 }
 
+
+inline int random_int(int min, int max)
+{
+	return min + rand() % (max - min + 1);
+}
 
 int main()
 {
@@ -171,51 +308,75 @@ int main()
 	char* ver = "0.0.2";
 	printf("Hill climbing algorithm version %s\n", ver);
 
-	//char* inputFilename = "C:\\Users\\Matz\\Desktop\\pd216-32.jpg";
-	char* inputFilename = "C:\\Users\\Matz\\Desktop\\statue.png";
+	char* inputFilename = "C:\\Temp\\Einar.png";
+	//char* inputFilename = "C:\\temp\\hierro.png";
+
 	int template_width, template_height, template_channels;
 	int png_channels = 4;
-	int bpp = 1; // bits per pixel
-	unsigned char *img_template = (unsigned char *)stbi_load(inputFilename, &template_width, &template_height, &template_channels, 0);
-	unsigned char *img_tmp = (unsigned char*)calloc(template_width*template_height*png_channels*bpp, sizeof(unsigned char*));
-	unsigned char *img_out = (unsigned char*)calloc(template_width*template_height*png_channels*bpp, sizeof(unsigned char*));
-	unsigned char *img_diff = (unsigned char*)calloc(template_width*template_height*png_channels*bpp, sizeof(unsigned char*));
+	int bpp = 1; // bits per pixel?
 
-	unsigned char *img_empty = (unsigned char*)calloc(template_width*template_height*png_channels*bpp, sizeof(unsigned char*));
+	//ignore template_channels
+	unsigned char* img_template = (unsigned char*) stbi_load(inputFilename, &template_width, &template_height, &template_channels, 4);
+	unsigned char* img_tmp = (unsigned char*) calloc(template_width * template_height * png_channels * bpp, sizeof(unsigned char));
+	//unsigned char *img_tmp = (unsigned char*) calloc(template_width*template_height*png_channels*bpp, sizeof(unsigned char*));
+	unsigned char* img_out = (unsigned char*) calloc(template_width * template_height * png_channels * bpp, sizeof(unsigned char));
+
+    template_channels = 4; // force 4 channels for output and calculations, ignore alpha in template if it exists
+	printf("template channels: %d\n", template_channels);
+
+	if (template_width * template_height * png_channels * bpp > INT_MAX)
+	{
+		printf("Image is too large\n");
+		return 1;
+	}
+	
+
 
 
 	if (img_template == NULL)
 	{
 		printf("Error in loading the image\n");
 		std::cin >> i;
-		exit(1);
+		return 1;
 	}
 
 	auto start = std::chrono::high_resolution_clock::now();
 	//todo: add as list of pe to output as svg
 	srand(time(NULL));
-	float difference_metric = 1000000000;
-	for (int i = 0; i < 500; i++) // add discs
+	float difference_metric = 1e30;
+
+	memset(img_tmp, 255, template_width * template_height * 4);
+	int x, y;
+
+	int red, green, blue;
+	int radius;
+	float best_metric_global = 1e30;
+
+	for (int i = 0; i < 1000; i++) // add discs
 	{
-		polyElement pe_tmp;
+		polyElement pe_tmp = polyElement();
 		polyElement pe_best;
-		pe_best.metric = 10000000000000;
+		pe_best.metric = 1e30;
 
+		int alpha;
+		int attempts = 0;
+	
+		// test just cycling colors
 
-		int alpha = min(rand() % 255, 255);
-
-		for (int j = 0; j < 1000; j++) // test adding a disc
+		for (int j = 0; j < 100; j++) // test adding a disc
 		{
-			int rad = 3 + rand() % 70;
-			int red = rand() % 255;
-			int green = rand() % 255;
-			int blue = rand() % 255;
-			int x = rad+rand() % (template_width-rad);
-			int y = rad+rand() % (template_height-rad);
+			x = random_int(0, template_width);
+			y = random_int(0, template_height);
 
+			radius = random_int(4, 50);
+			alpha = random_int(50, 255);
+			red = random_int(0, 255);
+			green = random_int(0, 255);
+			blue = random_int(0, 255);
 			pe_tmp.x = x;
 			pe_tmp.y = y;
-			pe_tmp.radius = rad;
+			
+			pe_tmp.radius = radius;
 
 			pe_tmp.col.r = red;
 			pe_tmp.col.g = green;
@@ -223,45 +384,34 @@ int main()
 			pe_tmp.col.a = alpha;
 
 			// add disc to tmp to calculate if the added disc is better
-			add_disc(img_tmp, png_channels,
-				template_width, template_height,
-				pe_tmp);
+			//test_fun(pe_tmp);
+			difference_metric = add_disc_diff_opt(img_template, img_tmp, template_channels, pe_tmp, template_width, template_height);
 
-			difference_metric = diff(img_template, img_tmp, png_channels,
-				template_width, template_height);
-
-			memcpy(img_tmp, img_out, template_width*template_height*png_channels*bpp * sizeof(unsigned char*));
-
-			if (difference_metric < pe_best.metric) // better diff
+			if (difference_metric < pe_best.metric)
 			{
-				//printf("diff: %f\n", difference_metric);
-				//printf("x: %d  y:%d %f\n", x, y, difference_metric);
 				pe_best.x = x;
 				pe_best.y = y;
-				pe_best.radius = rad;
-				pe_best.col.r = red;
-				pe_best.col.g = green;
-				pe_best.col.b = blue;
-				pe_best.col.a = alpha;
+				pe_best.radius = pe_tmp.radius;
+				pe_best.col.r = pe_tmp.col.r;
+				pe_best.col.g = pe_tmp.col.g;
+				pe_best.col.b = pe_tmp.col.b;
+				//pe_best.col.a = alpha;
+				pe_best.col.a = pe_tmp.col.a;
 				pe_best.metric = difference_metric;
-				//break;
+				attempts++;
 			}
 		}
 
 		// add the best disc to img_out (pe)
-		add_disc(img_out, png_channels,
+		add_disc(img_tmp, png_channels,
 			template_width, template_height,
 			pe_best);
 
-		printf("diff: %f\n", pe_best.metric);
-		printf("added disc: %d\n", i);
-
-		memcpy(img_tmp, img_out, template_height*template_width*png_channels*bpp * sizeof(unsigned char*));
-		if (i % 10 == 0)
+		if (i % 30 == 0)
 		{
-			stbi_write_png("C:\\Users\\Matz\\Desktop\\output_tmp.png", template_width, template_height, png_channels, img_out, template_width * png_channels);
+			stbi_write_png("C:\\temp\\output_tmp.png", template_width, template_height,
+				png_channels, img_tmp, template_width * png_channels);
 		}
-		//stbi_write_png("C:\\Users\\Matz\\Desktop\\output_template_temp.png", template_width, template_height, png_channels, img_template, template_width * png_channels);
 
 	}
 
@@ -270,23 +420,11 @@ int main()
 	std::chrono::duration<double> elapsed = finish - start;
 	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 
-	stbi_write_png("C:\\Users\\Matz\\Desktop\\output_new.png", template_width, template_height, png_channels, img_out, template_width * png_channels);
+	stbi_write_png("C:\\temp\\output_tmp.png", template_width, template_height, png_channels, img_tmp, template_width * png_channels);
 
 	printf("enter a number to quit\n");
 	std::cin >> i;
 
 	return 0;
 }
-
-
-//// add a gray image and modify its color, rotation and transparency
-//void add_image(unsigned char *img, unsigned char *shp, int channels, int img_width, int img_height, int x, int y, int radius, int n, int rotation, int r, int g, int b)
-//{
-//
-//
-//
-//
-//}
-
-
 
